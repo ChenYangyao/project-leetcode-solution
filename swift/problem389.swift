8840b62439f295b60633ddd66c6a744f36a916e8
Young WU <tsihyoung@gmail.com>
2018-10-15 19:47:03 +0800

    problem748 et 389 swift

---
+++ swift/problem389.swift
@@ -0,0 +1,54 @@
+import Dispatch
+
+extension String {
+    func sum() -> UInt32 {
+        var summation = UInt32(0)
+        for char in self.unicodeScalars {
+            summation += char.value
+        }
+        return summation
+    }
+    
+    //much slower than sum() since sum2() converts String.UnicodeScalarView to [UInt32] before summation
+    func sum2() -> UInt32 {
+        return self.unicodeScalars.map{$0.value}.reduce(0,+)
+    }
+}
+
+class Solution1 {
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        return Character(UnicodeScalar((t.sum() - s.sum()))!)
+    }
+}
+
+//about 30 times slower than Solution1. Sorting is time-consuming.
+class Solution2 {
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        let s_array = s.sorted()
+        let t_array = t.sorted()
+        for index in 0..<s_array.count {
+            if s_array[index] != t_array[index] {
+                return t_array[index]
+            }
+        }
+        return t_array.last!
+    }
+}
+
+protocol sol {
+    func findTheDifference(_ s: String, _ t: String) -> Character
+}
+extension Solution1: sol{}
+extension Solution2: sol{}
+
+func time_consuming<T: sol>(_ solution_class: T, _ name: String, _ r: Int) {
+    let time_start = DispatchTime.now().uptimeNanoseconds
+    for _ in 1...r {
+        _ = solution_class.findTheDifference("sjdklfwequirojaiunjkdbnakjlhquiewhtuashlkjdbvjakldshquiehgqwiglhaajksdbnvakldnvjnqiluhtqiuwehqilheaksjdbkqlehqui","sjdklfwequirojdaiunjkdbnakjlhquiewhtuashlkjdbvjakldshquiehgqwiglhaajksdbnvakldnvjnqiluhtqiuwehqilheaksjdbkqlehqui")
+    }
+    let time_end = DispatchTime.now().uptimeNanoseconds
+    print(name,(time_end-time_start)/1000000,"ms","for \(r) times")
+}
+
+time_consuming(Solution1(),"1",10000000)
+time_consuming(Solution2(),"2",10000000)
---
+++ swift/problem748.swift
@@ -0,0 +1,71 @@
+import Dispatch
+
+class Solution {
+    private let prime: Array<UInt> = [2, 3, 5, 7, 11, 13, 17] //the max length of licensePlate is 7
+    private var plate = [Int:UInt]()
+    
+    func shortestCompletingWord(_ licensePlate: String, _ words: [String]) -> String {
+        let eigenvalue = eigenmode(licensePlate)
+        var output = String(repeating: " ", count: 16)
+        for word in words {
+            if (word.count < output.count && eigen(word) % eigenvalue == 0) {
+                output = word
+            }
+        }
+        return output
+    }
+    
+    private func eigenmode(_ licensePlate: String) -> UInt {
+        let licensePlate = licensePlate.unicodeScalars
+        var count = 0
+        var eigenvalue = UInt(1)
+        for char in licensePlate {
+            let tmp = Int(char.value)
+            if (tmp >= 97) {
+                if (plate[tmp] == nil) {
+                    plate[tmp] = prime[count]
+                    count += 1
+                }
+                eigenvalue *= plate[tmp]!
+            } else if (tmp >= 65) {
+                if (plate[tmp+32] == nil) {
+                    plate[tmp+32] = prime[count]
+                    count += 1
+                }
+                eigenvalue *= plate[tmp+32]!
+            }
+        }
+        return eigenvalue
+    }
+    
+    private func eigen(_ word: String) -> UInt {
+        let word = word.unicodeScalars
+        var eigenvalue = UInt(1)
+        for char in word {
+            if let tmp = plate[Int(char.value)] {
+                eigenvalue *= tmp
+            }
+        }
+        return eigenvalue
+    }
+}
+
+let licensePlate = "Ih DTR2351"
+let words = ["first","second","third"]
+
+protocol sol {
+    func shortestCompletingWord(_ licensePlate: String, _ words: [String]) -> String
+}
+extension Solution: sol{}
+
+func time_consuming<T: sol>(_ solution_class: T, _ name: String, _ r: Int) {
+    let time_start = DispatchTime.now().uptimeNanoseconds
+    for _ in 1...r {
+        _ = solution_class.shortestCompletingWord(licensePlate, words)
+    }
+    let time_end = DispatchTime.now().uptimeNanoseconds
+    print(name,(time_end-time_start)/1000000,"ms","for \(r) times")
+}
+
+time_consuming(Solution(),"1",1000)
+print(Solution().shortestCompletingWord(licensePlate, words))
