70811e7a2b4faa28bf913e9504ec138ad70c2758
刘星辰 <liuxingchen@lxc.local>
2019-01-23 13:19:35 +0800

    default

---
+++ java/problem26.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int currentPointer=0;
+        int sweepPointer;
+        for(sweepPointer=0;sweepPointer<nums.length;sweepPointer++){
+            if(nums[sweepPointer]!=nums[currentPointer]){
+                nums[currentPointer+1]=nums[sweepPointer];
+                currentPointer++;
+            }
+        }
+        return currentPointer+1;
+        
+    }
+}
