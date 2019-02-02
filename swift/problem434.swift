c01c90999b016067c20dc50ddb76d5e8df3e9c60
Young WU <tsihyoung@gmail.com>
2019-02-02 21:47:02 +0800

    amendment

--- swift/problem434.swift
+++ swift/problem434.swift
@@ -1,12 +1,16 @@
 class Solution {
     func countSegments(_ s: String) -> Int {
-        var ans = 0; var prev = Character(" ")
+        var ans = 0; var prev = true
         let space = Character(" ")
         for char in s {
-            if (char != space && prev == space) {
-                ans += 1
+            if (char != space) {
+                if prev {
+                    ans += 1
+                }
+                prev = false
+            } else {
+                prev = true
             }
-            prev = char
         }
         return ans
     }
