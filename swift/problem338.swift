07c3430123285fa03d3486506a318058c164ef71
Young WU <tsihyoung@gmail.com>
2019-03-18 10:47:06 +0800

    amendment

--- swift/problem338.swift
+++ swift/problem338.swift
@@ -3,12 +3,8 @@ class Solution {
     func countBits(_ num: Int) -> [Int] {
         var bitCount = [Int](repeating: 0, count: num + 1)
         
-        if (num == 0) {
-            return bitCount
-        }
-        
-        for i in 1...num {
-            bitCount[i] = bitCount[i>>1]+i&1
+        for i in 0...num {
+            bitCount[i] = bitCount[i>>1] + i&1
         }
         
         return bitCount
